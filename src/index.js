af6e36e2ec3c1f6092b5a38aa037055626791854
Niko <luo.kun@seatell.cn>
2018-03-09 14:29:13 +0800

    router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-//根文件redux
+// //根文件redux
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Reducers from './reducers'
 let store = createStore(Reducers);
 
 ReactDOM.render(
+    <Provider store={store}>
         <App />
-    ,
-    document.getElementById('root')
-)
+    </Provider>,
+    document.getElementById('root'));
 
 // store.subscribe(render) 监听state的变化
