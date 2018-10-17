30c7ef31d88ff5ba8ac20fd497a07dec15a33efd
罗坤 <1498710037@qq.com>
2018-10-17 17:06:23 +0800

    mobx-react-boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import DevTools from "mobx-react-devtools";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import TodoList from "./components/TodoList";
+import TodoListModel from "./models/TodoListModel";
+import TodoModel from "./models/TodoModel";
+
+const store = new TodoListModel();
+
+render(
+  <div>
+    {/*<DevTools />*/}
+    <TodoList store={store} />
+  </div>,
+  document.getElementById("root")
+);
+
+store.addTodo("Get Coffee");
+store.addTodo("Write simpler code");
+store.todos[0].finished = true;
+
+setTimeout(() => {
+  store.addTodo("Get a cookie as well");
+}, 2000);
+
+// playing around in the console
+window.store = store;
