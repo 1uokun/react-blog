484e95b7d359256349353c5547c6092f902b208a
罗坤 <1498710037@qq.com>
2019-01-02 22:51:28 +0800

    设置过期时间

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,13 +3,21 @@ import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import expireReducer from 'redux-persist-expire'
 import rootReducer from './reducers/index'
 
 const loggerMiddleware = createLogger();
+const expireTransform = expireReducer('life',{
+    persistedAtKey:'loadedAt',
+    expireSeconds: 86400,//设置token时效为1天
+    autoExpire:false
+});
+//指定数据设置过期时间
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist:[]
+    blacklist:[],
+    transforms:[expireTransform]
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
--- src/reducers/life.js
+++ src/reducers/life.js
@@ -1,9 +1,10 @@
 import {CUT_LIFE, ADD_LIFE, LOADING_STATE} from "../actions";
+import moment from 'moment'
 
 export default function(state={},action){
     switch(action.type){
         case ADD_LIFE:
-            return {...action.payload};
+            return {...action.payload,loadedAt: moment(),};
         case CUT_LIFE:
             return {...action.payload};
         case LOADING_STATE:
