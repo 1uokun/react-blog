9cf90fdf882f128424a0f9d4d3fd13f571638be4
罗坤 <1498710037@qq.com>
2019-01-02 22:47:56 +0800

    数据持久化

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,28 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers/index'
 
 const loggerMiddleware = createLogger();
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist:[]
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export default function configureStore(preloadedState) {
-    return createStore(
-        rootReducer,
+    const store = createStore(
+        persistedReducer,
         preloadedState,
         applyMiddleware(
             thunkMiddleware,
             loggerMiddleware
         )
-    )
+    );
+    const persistor = persistStore(store);
+    return {store,persistor}
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import configureStore from './configureStore'
-const store = configureStore();
+const {store, persistor} = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
+        <PersistGate
+            // loading={<View style={{flex:1,backgroundColor:'red'}}><Text>Loading...</Text></View>}
+            persistor={persistor}
+        >
         <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
